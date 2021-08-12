0d436b8eb8a37c307be0867567d52ada8a64a7db
haruto0328 <sh20010328@gmail.com>
2021-08-13 00:12:33 +0900

    make it better

--- database.py
+++ database.py
@@ -13,12 +13,6 @@ from linebot.models import TextSendMessage
 line_bot_api = LineBotApi("miP/OnH4rBYGo5qLlP7pyxAXFdss80DswDHNXqdafSxJ0nQZlKyfcwpHsyhN5FPYlk5Shc6WRo8aGBzIpdzknC2KueOtNbB4SIslsIKZdaprgn0Tf8qxxXBDEt9WV4h/lF/7tN0q1jpcQ7VcesfqHQdB04t89/1O/w1cDnyilFU=")
 handler = WebhookHandler("e157aafd232b4650558937ce7250f4d9")
 
-def default(event):
-            line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage('今日の時から、活動があります。忘れずに参加してください！！'))
-
-
 conn = sqlite3.connect('iungoback.db')
 c = conn.cursor()
 
@@ -28,9 +22,10 @@ for row in c.execute('SELECT datetime FROM dates where rowid = last_insert_rowid
     plan = re.search('\d+-\d+-\d+', str(row)).group()
     today = str(datetime.date.today())
     if(plan == today):
-        # print('今日が活動日です。')
-        @handler.default()
+        @handler.default_2()
+        def default_2(event):
+            line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage('今日の時から、活動があります。忘れずに参加してください！！'))
 
-
-# データベースへのアクセスが終わったら close する
 conn.close()
