19c08db2e7c3abd9fc8ed6eba4667228ec69436d
haruto0328 <sh20010328@gmail.com>
2021-08-13 00:01:25 +0900

    make it better

--- database.py
+++ database.py
@@ -13,6 +13,11 @@ from linebot.models import TextSendMessage
 line_bot_api = LineBotApi("miP/OnH4rBYGo5qLlP7pyxAXFdss80DswDHNXqdafSxJ0nQZlKyfcwpHsyhN5FPYlk5Shc6WRo8aGBzIpdzknC2KueOtNbB4SIslsIKZdaprgn0Tf8qxxXBDEt9WV4h/lF/7tN0q1jpcQ7VcesfqHQdB04t89/1O/w1cDnyilFU=")
 handler = WebhookHandler("e157aafd232b4650558937ce7250f4d9")
 
+def default(event):
+            line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage('今日の時から、活動があります。忘れずに参加してください！！'))
+
 
 conn = sqlite3.connect('iungoback.db')
 c = conn.cursor()
@@ -25,10 +30,6 @@ for row in c.execute('SELECT datetime FROM dates where rowid = last_insert_rowid
     if(plan == today):
         # print('今日が活動日です。')
         @handler.default()
-        def default(event):
-            line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage('今日の時から、活動があります。忘れずに参加してください！！'))
 
 
 # データベースへのアクセスが終わったら close する
