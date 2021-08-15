b115b91823ea00cdf94588bca93aef4470f9db58
haruto0328 <sh20010328@gmail.com>
2021-08-15 15:56:01 +0900

    make it better

--- database.py
+++ database.py
@@ -23,14 +23,14 @@ c = conn.cursor()
 
 # c.execute("INSERT INTO dates VALUES ('2021-08-12T09:00')")
 
-row = c.execute('SELECT dates FROM datetimes ORDER BY id DESC LIMIT 1')
-plan = re.search('\d+-\d+-\d+', str(row)).group()
-today = str(datetime.date.today())
-if(plan == today):
-    @handler.default()
-    def default(event):
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text='今日の時から、活動があります。忘れずに参加してください！！'))
+for row in c.execute('SELECT dates FROM datetimes ORDER BY id DESC LIMIT 1;'):
+    plan = re.search('\d+-\d+-\d+', str(row)).group()
+    today = str(datetime.date.today())
+    if(plan == today):
+        @handler.default()
+        def default(event):
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text='今日の時から、活動があります。忘れずに参加してください！！'))
 
 conn.close()
