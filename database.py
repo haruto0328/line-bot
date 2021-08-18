5eedb7ef677cf73d6b26e6787fec45de50ea9a34
haruto0328 <sh20010328@gmail.com>
2021-08-18 23:31:41 +0900

    make it better

--- database.py
+++ database.py
@@ -21,14 +21,13 @@ conn = psycopg2.connect("host=" + "ec2-54-197-100-79.compute-1.amazonaws.com" +
                             " password=" + "24d6a2537ae752fc37baa19b3463e8e09c13732e60b26966afec049323e57c5e")
 c = conn.cursor()
 
-# c.execute("INSERT INTO dates VALUES ('2021-08-12T09:00')")
-
 c.execute('SELECT dates FROM datetimes ORDER BY id DESC LIMIT 1')
 for row in c:
     plan = re.search('\d+-\d+-\d+', str(row)).group()
+    plan_time = re.search('\d+:\d+', str(row)).group()
     today = str(datetime.date.today())
     if(plan == today):
-        line_bot_api.push_message('U888983a63d766bb9a7e716ca8ca7f8b9', messages=TextSendMessage(text='今日の時から、活動があります。忘れずに参加してください！！'))
+        line_bot_api.push_message('U888983a63d766bb9a7e716ca8ca7f8b9', messages=TextSendMessage(text='今日の'+plan_time+'時から、活動があります。忘れずに参加してください！！'))
         # @handler.default()
         # def default():
         #     # line_bot_api.reply_message(
